8fd9610e19a8383ba16aa45d80be0cf5c9845b6f
Yevhenii Shablia <59193602+Jekaprio@users.noreply.github.com>
2023-09-21 23:24:23 +0300

    Добавьте файлы проекта.

---
+++ Models/CryptoModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Windows;
+
+public class CryptoModel
+{
+    private readonly HttpClient _httpClientCoinCap;
+   
+
+    public CryptoModel()
+    {
+        _httpClientCoinCap = new HttpClient();
+        _httpClientCoinCap.BaseAddress = new Uri("https://api.coincap.io/v2/assets");
+    }
+
+   
+    public async Task<string> GetCoinCapdataAsync()
+    {
+        try
+        {
+            HttpResponseMessage response = await _httpClientCoinCap.GetAsync("https://api.coincap.io/v2/assets");
+            response.EnsureSuccessStatusCode();
+            string data = await response.Content.ReadAsStringAsync();
+            return data;
+        }
+        catch(HttpRequestException ex)
+        {        
+            return ex.Message;
+        }
+    }
+  
+}
+
+
+    
+
+
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Crypto_V2.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Crypto_V2
+{
+    public partial class MainWindow : Window
+    {
+        public readonly HttpClient _httpClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient();
+
+        }
+
+        private void CheckApiStatus(object sender, RoutedEventArgs e)  // Перевірка працездатності API 
+        {
+            string apiUrl = "https://api.coincap.io/v2/assets";
+
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    string response = client.DownloadString(apiUrl);
+                    MessageBox.Show("Статус API: API праює нормально (HTTP 200 OK)", "Статус API", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Статус API: Виникла помилка при доступі до API", "Статус API", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+      
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
+
